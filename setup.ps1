29098ed8a413352103e96cbbf8fc1210c9fc268b
simpe00 <peppel.simon@gmail.com>
2021-02-22 16:23:25 +0100

    restruct

--- setup.ps1
+++ setup.ps1
@@ -4,13 +4,13 @@
 $TestVar = $(get-location).Path+"\venv\"
 #echo $TestVar
 if (-not (Test-Path $TestVar)) {
-    echo "creat virtual enviroment"
+    Write-Output "creat virtual enviroment"
     python3 -m venv $TestVar
-    echo "venv: done"
+    Write-Output "venv: done"
 
     .\venv\Scripts\activate
     
-    echo "install python packages via pip"
+    Write-Output "install python packages via pip"
     pip install -r requirements.txt
-    echo "installed packages"
+    Write-Output "installed packages"
 } 
